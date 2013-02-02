cb51a74c355ca56e1d96a093d84756742205e40e
Michael Penkov <misha.penkov@gmail.com>
2013-02-02 23:13:17 +0900

    Updated parse script to use regular expressions.

--- parse.py
+++ parse.py
@@ -1,32 +1,33 @@
 """Attempt to parse the primitives for each Kanji."""
+import re
+
+with open("primitives.txt") as fin:
+    primitives = set(fin.read().split("\n"))
 
 with open("layout.txt") as fin:
-    chunks = fin.read().split("\n\n")
+    lines = fin.read().split("\n")
 
-def parse_chunk(chunk, prev_num=0):
-    """Parse a chunk given the chunk string and the previous Kanji number.
-    Returns the parsed Kanji number, keyword, and list of contained primitives (may be empty).
-    If nothing is found, returns None."""
-    chunk = chunk.strip()
-    lines = chunk.split("\n")
-    header = filter(None, lines[0].split(" "))
-    if len(header) != 2:
-        return None
-    number = header[0]
-    keyword = header[1]
-    print number, keyword
-    if prev_num > 0 and number != prev_num + 1:
-        # bad Heisig number
-        return None
-    return number, keyword, list()
-
-parsed = dict()
-prev_num = 0
-for i, chunk in enumerate(chunks):
-    if i > 10:
-        break
-    result = parse_chunk(chunk, prev_num)
-    if not result:
+regex = re.compile(r"^ {2,}(?P<heisig_number>\d+) {40,}(?P<keyword>[\w ]+)$")
+keywords = dict()
+stories = dict()
+for l in lines:
+    m = regex.match(l)
+    if m:
+        heisig_number = int(m.group("heisig_number"))
+        keyword = m.group("keyword")
+        keywords[heisig_number] = keyword
+        stories[heisig_number] = set()
+        #print heisig_number, keyword
         continue
-    prev_num += 1
-    print i, result
+    #
+    # this doesn't work too well for a couple of reasons:
+    # 1) some primitives are ngrams, so splitting by a space doesn't capture them
+    # 2) not all primitives that occur in the story are part of the current Kanji;
+    #    they can just be part of the text
+    #
+    for prim in set(filter(lambda w: w and w.lower() in primitives, l.lower().split(" "))):
+        stories[heisig_number].add(prim)
+
+for heisig in sorted(stories):
+    print heisig, keywords[heisig]
+    print "\t", "\n\t".join(stories[heisig])
